1ba6fc8dded636a4660964fceae13c6370e82bce
Iiro Hietala <iiro.hietala@gofore.com>
2018-07-30 17:02:07 +0300

    Removed test data creation

--- src/main/kotlin/org/silverduck/clucklin/service/ClucklingService.kt
+++ src/main/kotlin/org/silverduck/clucklin/service/ClucklingService.kt
@@ -1,14 +1,11 @@
 package org.silverduck.clucklin.service
 
-import org.silverduck.clucklin.data.Breed
 import org.silverduck.clucklin.data.Cluckling
-import org.silverduck.clucklin.data.Gender
 import org.silverduck.clucklin.repository.ClucklingRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
-import javax.annotation.PostConstruct
 
 /**
  * @author <a href="mailto:iiro.hietala@gofore.com">Iiro Hietala</a>
@@ -21,11 +18,4 @@ class ClucklingService(val clucklingRepository: ClucklingRepository) {
     fun removeCluckling(id: Long) = clucklingRepository.deleteById(id)
 
     fun list() : Flux<Cluckling> = clucklingRepository.findAll();
-
-    @PostConstruct
-    fun createTestData() {
-        clucklingRepository.save(Cluckling(1, "Matti", 33, Gender.MALE, Breed("Royal Cluckson")))
-        clucklingRepository.save(Cluckling(2, "Torsti", 23, Gender.MALE, Breed("Furious Bloodling")))
-    }
-
 }
